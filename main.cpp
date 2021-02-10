8c64c97a0ce237a95b08d09abce71f86fd08a73b
Clément Strauss <clement.strauss@gmail.com>
2021-02-10 10:56:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,27 +14,10 @@ main()
 {
   using namespace StreamFlow;
 
-  //    auto producer_ptr = Factory::make_ptr("producer");
-  //    auto consumer_ptr = Factory::make_ptr("consumer");
-
-  //    //connect component together
-  //    (*producer_ptr)["out"].connect((*consumer_ptr)["in"]);
-
-  //    //connect component together with operator
-  //    (*producer_ptr)["out"]+(*consumer_ptr)["in"];
-
-  //    // spawn new thread
-  //    std::thread firstp (&RLNM::ComponentBase::loop, producer_ptr.get());
-  //    std::thread secondc (&RLNM::ComponentBase::loop, consumer_ptr.get());
-  //    firstp.join();
-  //    secondc.join();
-
   std::cout << StreamFlow::Factory::describe() << std::endl;
 
-  //   return 1;
-
-  Application app("put application name here");
-  // create Component (or node) in the app
+   Application app("put application name here");
+  // add Component (or node) in the app
   app.addNode("producer");
   app.addNode("consumer");
 
@@ -57,23 +40,6 @@ main()
 
   // run the application, enjoy pipeline and parallelism;
   app.run();
-
-  //    RLNM::producerComponent producerComponent;
-  //    RLNM::consumerComponent consumerComponent;
-
-  //    producerComponent["out"]+(consumerComponent["in"]);
-
-  //    std::cout << producerComponent.describe() << std::endl;
-  //    std::cout << consumerComponent.describe() << std::endl;
-
-  //    producerComponent.out.write(10000);
-  //    auto value = consumerComponent.in.read();
-  //    std::cout << "read value " << value << std::endl;
-
-  //    std::thread first (&RLNM::producerComponent::loop, &producerComponent);
-  //    std::thread second (&RLNM::consumerComponent::loop, &consumerComponent);     // spawn new thread that calls foo()
-  //    second.join();
-  //    first.join();
 
   return 0;
 }
